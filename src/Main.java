dceb4c47a1050d63805d5a551a3225b814443539
Abil N George <abilngeorge@gmail.com>
2013-08-30 10:02:31 +0530

    Added:Threads in edits

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
 	public static void main(String[] args){
 		// TODO Auto-generated method stub\
-		String str = "belive";
+		String str = "comitte";
 		dict = new Dictionary();
 		WordCheck wc = new WordCheck(dict);
 		if(! dict.hasWord(str)){
